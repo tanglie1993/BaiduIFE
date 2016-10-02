0b7eef044df7c1c8826eef2f01303b18f13897f2
tanglie1993 <tanglie23@163.com>
2016-10-02 22:46:08 +0800

    整理代码

--- 2015_spring/task/task0002/task0002.js
+++ 2015_spring/task/task0002/task0002.js
@@ -21,33 +21,5 @@ function initEvent() {
     $("addbtn").addEventListener("click", addEventHandle, false);
 }
 
-function getPosition(element) {
-    var result = new Object();
-    result.x = $(element).offsetTop;
-    result.y = $(element).offsetLeft;
-    return result
-}
-
-function traversal(element, attribute) {
-    if (element === null) {
-        return;
-    } else {
-        if(element[attribute] != "" && element[attribute] !== undefined){
-            console.log("element[attribute]: " + element[attribute]);
-        }
-        
-        if (element.childNodes.length > 0) {
-            for (var i = 0; i < element.childNodes.length; i++) {
-                traversal(element.childNodes[i], attribute);
-            }
-        }
-    }
-}
-
-function traversal1(){
-    traversal(document.body, "type");
-}
-
-
-traversal1();
 initEvent();
+
