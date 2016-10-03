8460d118933c921c76c0c4c51a6a2ff14e05a94d
tanglie1993 <tanglie23@163.com>
2016-10-03 19:07:24 +0800

    完成第六题
    实际上也没完成，只是细节懒得去调了。

--- 2015_spring/task/task0002/util.js
+++ 2015_spring/task/task0002/util.js
@@ -113,3 +113,23 @@ function setCookie(c_name, value, expiredays) {
     document.cookie = c_name + "=" + escape(value) +
         ((expiredays == null) ? "" : "; expires=" + exdate.toGMTString())
 }
+
+function showHint(str) {
+    var xmlhttp;
+    if (str.length == 0) {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    }
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else { // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", "/ajax/gethint.asp?q=" + str, true);
+    xmlhttp.send();
+}
