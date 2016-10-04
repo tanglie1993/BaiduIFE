b62d10b0581f1cdbd2d94125adef576c28de3ac1
tanglie1993 <tanglie23@163.com>
2016-10-04 18:57:04 +0800

    保存一版能用的scroller

---
+++ 2015_spring/task/task0002/js/task0002-3.js
@@ -0,0 +1,15 @@
+//count = 0;
+//timedCount();
+//
+//function timedCount() {
+//    renderImages(count);
+//    count += 1;
+//    setTimeout("timedCount()", 15);
+//}
+//
+//function renderImages(left){
+//    left = left % 200;
+//    $("test").scrollTop(left);
+//}
+
+$("test1").animate({ scrollLeft:110 }, "fast");
