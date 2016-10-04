5088243a370385bb2a53014dbfa64f865b35131f
tanglie1993 <tanglie23@163.com>
2016-10-04 21:21:54 +0800

    实现拖动

--- 2015_spring/task/task0002/js/task0002-5.js
+++ 2015_spring/task/task0002/js/task0002-5.js
@@ -1,20 +1,47 @@
-function mouseMove(ev) {
-            Ev = ev || window.event;
-            var mousePos = mouseCoords(ev);
-            document.getElementById("xxx").value = mousePos.x;
-            document.getElementById("yyy").value = mousePos.y;
-        }
+var isDragging = false;
+var offsetX = 0;
+var offsetY = 0;
 
-        function mouseCoords(ev) {
-            if (ev.pageX || ev.pageY) {
-                return {
-                    x: ev.pageX,
-                    y: ev.pageY
-                };
-            }
-            return {
-                x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-                y: ev.clientY + document.body.scrollTop - document.body.clientTop
-            };
-        }
-        document.onmousemove = mouseMove;
+function mouseMove(ev) {
+    if (isDragging) {
+        Ev = ev || window.event;
+        var mousePos = mouseCoords(ev);
+        document.getElementById("xxx").value = mousePos.x;
+        document.getElementById("yyy").value = mousePos.y;
+        document.getElementById("pic").style.top = mousePos.y - offsetY;
+        document.getElementById("pic").style.left = mousePos.x - offsetX;
+    }
+}
+
+function mouseDown(ev) {
+    if (!isDragging) {
+        Ev = ev || window.event;
+        var mousePos = mouseCoords(ev);
+        var top = document.getElementById("pic").offsetTop;
+        var left = document.getElementById("pic").offsetLeft;
+        document.getElementById("xxx").value = mousePos.x - left;
+        document.getElementById("yyy").value = mousePos.y - top;
+        offsetX = mousePos.x - left;
+        offsetY = mousePos.y - top;
+        isDragging = true;
+    } else {
+        isDragging = false;
+    }
+
+}
+
+function mouseCoords(ev) {
+    if (ev.pageX || ev.pageY) {
+        return {
+            x: ev.pageX,
+            y: ev.pageY
+        };
+    }
+    return {
+        x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
+        y: ev.clientY + document.body.scrollTop - document.body.clientTop
+    };
+}
+
+document.onmousemove = mouseMove;
+document.getElementById("pic").onmousedown = mouseDown;
