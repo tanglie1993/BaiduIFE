080b708e7e1c32b17cb0a8739ec2a1e170e8c3e6
tanglie1993 <tanglie23@163.com>
2016-10-16 20:07:11 +0800

    稍微调一下UI
    onmouseover问题是一个浏览器兼容问题。暂且忽略吧……

--- 2015_spring/task/task0003/todo/ProjectsListInitiator.js
+++ 2015_spring/task/task0003/todo/ProjectsListInitiator.js
@@ -1,5 +1,3 @@
-
-
 function fillProjectsList() {
     while ($("projectList").hasChildNodes()) {
         $("projectList").removeChild($("projectList").lastChild);
@@ -37,24 +35,24 @@ function addFolderItemHeader(folderItem, name, id) {
     deleteImage.className = "categoryDeleteImage";
     deleteImage.src = "img/delete_project.png";
     deleteImage.onclick = function () {
-        if(isInEditMode){
+        if (isInEditMode) {
             return;
         }
         var isDeleteConfirmed = confirm("是否确定删除？");
         if (isDeleteConfirmed == true) {
             deleteFolder(id);
-            if(select(selectedProjectId) === null){
+            if (select(selectedProjectId) === null) {
                 selectedProject = null;
                 selectedProjectId = null;
             }
             var selectedTask = null;
-            if(selectedItem !== null){
+            if (selectedItem !== null) {
                 selectedTask = select(selectedItem.id);
             }
             fillProjectsList();
-            if(selectedTask != null){
+            if (selectedTask != null) {
                 fillTasksList(selectedTasksType);
-            }else{
+            } else {
                 selectedItem = null;
                 selectedProject = null;
                 selectedProjectId = null;
@@ -70,34 +68,48 @@ function addFolderItemHeader(folderItem, name, id) {
     addImage.className = "categoryAddImage";
     addImage.src = "img/add_project.png";
     addImage.onclick = function () {
-        if(isInEditMode){
+        if (isInEditMode) {
             return;
         }
         var projectName = prompt("请输入项目名称");
-        if(projectName !== null){
+        if (projectName !== null) {
             addProject(projectName, id);
-            fillProjectsList();   
+            fillProjectsList();
         }
     };
     addImage.style.visibility = "hidden";
     categoryNameDiv.appendChild(addImage);
 
     categoryNameDiv.appendChild(categoryNameText);
-    categoryNameDiv.onmouseover = function () {
-        if(isInEditMode){
-            return;
+    categoryNameDiv.onmouseover = function (e) {
+        if (!e) e = window.event;
+        var reltg = e.relatedTarget ? e.relatedTarget : e.fromElement;
+        while (reltg && reltg != this) reltg = reltg.parentNode;
+        if (reltg != this) {
+            // 这里可以编写 onmouseenter 事件的处理代码
+            console.log('111');
+            if (isInEditMode) {
+                return;
+            }
+            categoryNameDiv.className = "categoryNameDivSelected";
+            deleteImage.style.visibility = "visible";
+            addImage.style.visibility = "visible";
         }
-        categoryNameDiv.className = "categoryNameDivSelected";
-        deleteImage.style.visibility = "visible";
-        addImage.style.visibility = "visible";
     }
-    categoryNameDiv.onmouseout = function () {
-        if(isInEditMode){
-            return;
+    categoryNameDiv.onmouseout = function (e) {
+        if (!e) e = window.event;
+        var reltg = e.relatedTarget ? e.relatedTarget : e.toElement;
+        while (reltg && reltg != this) reltg = reltg.parentNode;
+        if (reltg != this) {
+            // 这里可以编写 onmouseleave 事件的处理代码
+            console.log('2222');
+            if (isInEditMode) {
+                return;
+            }
+            categoryNameDiv.className = "categoryNameDiv";
+            deleteImage.style.visibility = "hidden";
+            addImage.style.visibility = "hidden";
         }
-        categoryNameDiv.className = "categoryNameDiv";
-        deleteImage.style.visibility = "hidden";
-        addImage.style.visibility = "hidden";
     }
     folderItem.appendChild(categoryNameDiv);
 }
@@ -118,7 +130,7 @@ function addProjectItem(folderItem, name, projectId) {
     projectName.deleteImage.className = "projectDeleteImage";
     projectName.deleteImage.src = "img/delete_project.png";
     projectName.deleteImage.onclick = function () {
-        if(isInEditMode){
+        if (isInEditMode) {
             return;
         }
         var r = confirm("是否确定删除？");
@@ -129,18 +141,18 @@ function addProjectItem(folderItem, name, projectId) {
     };
     projectName.deleteImage.style.visibility = "hidden";
     projectName.appendChild(projectName.deleteImage);
-    projectName.onclick = function(){
-        if(isInEditMode){
+    projectName.onclick = function () {
+        if (isInEditMode) {
             return;
         }
-        if(projectName.selected == false){
+        if (projectName.selected == false) {
             projectName.className = "projectNameSelected";
             projectName.deleteImage.style.visibility = "visible";
             projectName.selected = true;
-            if(selectedProject === null){
-               selectedProject = projectName;
-               selectedProjectId = projectId;
-            }else{
+            if (selectedProject === null) {
+                selectedProject = projectName;
+                selectedProjectId = projectId;
+            } else {
                 selectedProject.className = "projectName";
                 selectedProject.selected = false;
                 selectedProject.deleteImage.style.visibility = "hidden";
@@ -148,7 +160,7 @@ function addProjectItem(folderItem, name, projectId) {
                 selectedProjectId = projectId;
             }
             fillTasksList(selectedTasksType);
-        }else{
+        } else {
             projectName.className = "projectName";
             projectName.selected = false;
             projectName.deleteImage.style.visibility = "hidden";
@@ -158,23 +170,23 @@ function addProjectItem(folderItem, name, projectId) {
         }
     }
     projectName.onmouseover = function () {
-        if(isInEditMode){
+        if (isInEditMode) {
             return;
         }
-        if(projectName.selected == false){
+        if (projectName.selected == false) {
             projectName.className = "projectNameSelected";
             projectName.deleteImage.style.visibility = "visible";
         }
     }
     projectName.onmouseout = function () {
-        if(isInEditMode){
+        if (isInEditMode) {
             return;
         }
-        if(projectName.selected == false){
+        if (projectName.selected == false) {
             projectName.className = "projectName";
             projectName.deleteImage.style.visibility = "hidden";
         }
-        
+
     }
     folderItem.appendChild(projectName);
 }
